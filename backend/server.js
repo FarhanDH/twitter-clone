2b7f6373cb9d4776a9e2cc46e3fde5d39027ece8
Farhan Dwi Hartanto <farhandwihartanto@gmail.com>
2024-05-19 18:16:46 +0800

    set production

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
 
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production" || process.env.NODE_ENV === "Production") {
 	app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 	app.get("*", (req, res) => {
